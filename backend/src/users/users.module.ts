60e35dda86ac74567d7980cc451fbaaa08733bd4
masonnguyen1243 <meowjcisgod@gmail.com>
2025-07-23 12:19:17 +0700

    update import

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { UsersService } from '@/users/users.service';
+import { UsersController } from '@/users/users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schemas/user.schema';
+import { User, UserSchema } from '@/users/schemas/user.schema';
 
 @Module({
   imports: [
