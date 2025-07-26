c70af5152e0ffc5333dcfc9de923b97665f278c3
masonnguyen1243 <meowjcisgod@gmail.com>
2025-07-26 13:36:33 +0700

    Config next-auth

--- frontend/src/components/auth/login.tsx
+++ frontend/src/components/auth/login.tsx
@@ -2,9 +2,23 @@
 import { Button, Col, Divider, Form, Input, Row } from "antd";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import Link from "next/link";
+import { authenticate } from "@/utils/actions";
 
 const Login = () => {
-  const onFinish = async (values: any) => {};
+  const onFinish = async (values: any) => {
+    const { email, password } = values;
+    //trigger sign-in
+    const res = await authenticate(email, password);
+    console.log(">>> check res", res);
+
+    // const data = await signIn("credentials", {
+    //   email,
+    //   password,
+    //   redirect: false,
+    // });
+
+    // console.log(">>> check data", data);
+  };
 
   return (
     <Row justify={"center"} style={{ marginTop: "30px" }}>
